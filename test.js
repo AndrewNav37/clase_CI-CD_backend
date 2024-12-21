cf26e9dbc607cc0d337945241e4ded7c9507ef4a
Andres Navarro <andresgabriel24@gmail.com>
2024-12-21 07:59:45 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+    
+    <title>Hola Mundo
+    <script>
+        // Mostrar mensaje con una alerta
+        alert('Hola Mundo');
+
+        // Mostrar el mensaje en el documento HTML
+        document.body.innerHTML = '
+
+Hola Mundo';
